011c02d519cecf40908507f04be36c21ca25f239
Tyler Reinecke <tylerreinecke@gmail.com>
2019-03-10 19:49:26 -0700

    init commit

---
+++ Pods/Target Support Files/Pods-MessageMyFriends/Pods-MessageMyFriends-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessageMyFriends : NSObject
+@end
+@implementation PodsDummy_Pods_MessageMyFriends
+@end
---
+++ Pods/Target Support Files/Pods-MessageMyFriendsTests/Pods-MessageMyFriendsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessageMyFriendsTests : NSObject
+@end
+@implementation PodsDummy_Pods_MessageMyFriendsTests
+@end
